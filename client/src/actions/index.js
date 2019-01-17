7490a839da49287bb87ad5bc8289b5a70b4c4c29
Joe Hinkley <joehhinkley@gmail.com>
2019-01-17 14:39:31 -0800

    created searchActions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,2 +1,3 @@
 export * from "./authActions";
 export * from "./subscriptionActions";
+export * from "./searchActions";
---
+++ client/src/actions/searchActions.js
@@ -0,0 +1,7 @@
+export const searchVideos = () => async dispatch =>{
+
+}
+
+export const searchChannels = () => async dispatch => {
+  
+}
