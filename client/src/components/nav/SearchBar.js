52e4aa2ae0e0f5c4707ec74654acc16537001243
Joe Hinkley <joehhinkley@gmail.com>
2019-01-17 14:34:25 -0800

    added comments to SearchBar

--- client/src/components/nav/SearchBar.js
+++ client/src/components/nav/SearchBar.js
@@ -9,7 +9,9 @@ class SearchBar extends Component {
     }
   }
   handleSearch(event) {
+    // stop page from refreshing when form is submitted
     event.preventDefault();
+    // redirect user to '/search/whateverTheyTypedIn'
     this.props.history.push(`/search/${this.state.searchTerm}`);
   }
   render() {
@@ -17,7 +19,9 @@ class SearchBar extends Component {
       <form onSubmit={this.handleSearch.bind(this)}>
         <input
           type="text"
+          // value based on state
           value={this.state.searchTerm}
+          // change state when something typed in
           onChange={event => this.setState({ searchTerm: event.target.value })}
         />
         <input type="submit" value="Search"/>
@@ -26,5 +30,5 @@ class SearchBar extends Component {
   }
 }
 
-
+// export with withRouter so this.props.history is available
 export default withRouter(SearchBar);
