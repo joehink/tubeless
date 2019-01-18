9547410585fde82d67d6f66eac2b4680091c0978
Joe Hinkley <joehhinkley@gmail.com>
2019-01-18 14:31:03 -0800

    added subscribe or unsubscribe button to each channel search result

--- client/src/components/search/ChannelResultList.js
+++ client/src/components/search/ChannelResultList.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
 class ChannelResultList extends Component {
+  subscribeOrUnsubscribe(resultId) {
+    // If subscription fetch has finished
+    if (this.props.subscriptions) {
+      // See if the ID of any channel the user is subscribed to matches the channel search result
+      const isSubscribed = this.props.subscriptions.some(sub => {
+        return sub.snippet.resourceId.channelId === resultId;
+      })
+
+      // if the user has the channel in their subscriptions
+      if (isSubscribed) {
+        // return a button that allows the user to unsubscribe
+        return <button>Unsubscribe</button>
+      } else {
+        // return a button that allows the user to subscribe
+        return <button>Subscribe</button>
+      }
+    }
+  }
   renderResults() {
     return this.props.results.map(result => {
       return (
@@ -10,6 +28,7 @@ class ChannelResultList extends Component {
             alt={result.snippet.channelTitle}
           />
           <h4>{result.snippet.channelTitle}</h4>
+          {this.subscribeOrUnsubscribe(result.snippet.channelId)}
         </div>
       )
     });
