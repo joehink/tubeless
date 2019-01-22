0bdcba883828688004b70f26ee56d6ce64fd63a2
Joe Hinkley <joehhinkley@gmail.com>
2019-01-21 20:54:23 -0800

    added comments to channelHeader

--- client/src/components/channel/ChannelHeader.js
+++ client/src/components/channel/ChannelHeader.js
@@ -3,7 +3,9 @@ import React from "react";
 const ChannelHeader = (props) => {
   return (
     <div>
+      // channel thumbnail image
       <img src={props.thumbnail} alt={props.title} />
+      // name of channel
       <p>{props.title}</p>
     </div>
   );
