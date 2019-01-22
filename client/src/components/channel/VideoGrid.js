be16db9e2a38e5f8d67be1976c9c073abaa122f7
Joe Hinkley <joehhinkley@gmail.com>
2019-01-21 21:03:28 -0800

    added comments to VideoGrid

--- client/src/components/channel/VideoGrid.js
+++ client/src/components/channel/VideoGrid.js
@@ -3,6 +3,8 @@ import VideoGridItem from "./VideoGridItem";
 
 class VideoGrid extends Component {
   renderVideos() {
+    // map through channel videos
+    // render one VideoGridItem for each video
     return this.props.videos.map(video => {
       return <VideoGridItem
                 key={video.id}
