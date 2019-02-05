ae03c0b75a62982a9c9d7d5e8beea8813b0736c4
Joe Hinkley <joehhinkley@gmail.com>
2019-02-04 20:56:55 -0800

    create sideBarActions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,3 +3,4 @@ export * from "./subscriptionActions";
 export * from "./searchActions";
 export * from "./channelActions";
 export * from "./videoActions";
+export * from "./sideBarActions";
---
+++ client/src/actions/sideBarActions.js
@@ -0,0 +1,1 @@
+export const toggleSideBar = () => ({ type: TOGGLE_SIDE_BAR });
