d4fcf5184483e968914268a9ab959ac41d58d67b
Joe Hinkley <joehhinkley@gmail.com>
2019-02-04 22:19:42 -0800

    close side bar in mobile when channel clicked

--- client/src/components/common/list/ListItem.js
+++ client/src/components/common/list/ListItem.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom"
+import { closeSideBar } from "../../../actions";
 
 class ListItem extends Component {
+  shouldCloseSideBar() {
+    if (window.innerWidth < 768) {
+      this.props.closeSideBar();
+    }
+  }
   renderThumbnail() {
     // If a thumbnail is provided
     if (this.props.thumbnail) {
@@ -15,7 +22,11 @@ class ListItem extends Component {
   render() {
     return (
       <li>
-        <Link to={this.props.link} className="list-item">
+        <Link
+          to={this.props.link}
+          className="list-item"
+          onClick={this.shouldCloseSideBar.bind(this)}
+        >
           { this.renderThumbnail() }
           <span>{ this.props.title }</span>
         </Link>
@@ -24,4 +35,4 @@ class ListItem extends Component {
   }
 }
 
-export default ListItem;
+export default connect(null, { closeSideBar })(ListItem);
