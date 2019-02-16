dea984ad7d9f024b9b8ac5f7236dd65bb3a90c09
Joe Hinkley <joehhinkley@gmail.com>
2019-02-16 13:45:12 -0800

    added comments to VideoDescription

--- client/src/components/video/VideoDescription.js
+++ client/src/components/video/VideoDescription.js
@@ -8,9 +8,13 @@ class VideoDescription extends Component {
     }
   }
   renderDescription(description) {
+    // regEx for finding URLs
     const urlRegex = /(?:^|[^"'])((ftp|http|https|file):\/\/[\S]+(\b|$))/gi
 
+    // replace all URLs with anchor tags
     let descriptionWithLinks = description.replace(urlRegex, " <a href='$1' target=_blank>$1</a>");
+
+    // return description with clickable URLs
     return <p
               className={this.state.showMore ? "more" : "less"}
               dangerouslySetInnerHTML={{__html: descriptionWithLinks}}
