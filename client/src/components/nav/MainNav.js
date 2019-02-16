3ee68c6284810f9f357316b78b8e697b468497cf
Joe Hinkley <joehhinkley@gmail.com>
2019-02-16 13:27:18 -0800

    added comments to MainNav

--- client/src/components/nav/MainNav.js
+++ client/src/components/nav/MainNav.js
@@ -6,11 +6,16 @@ import SearchBar from "./SearchBar";
 
 class MainNav extends Component {
   componentDidMount() {
+    // when in mobile, the sidebar should be hidden by default
     if (window.innerWidth < 768) {
         this.props.closeSideBar()
     }
+
+    // add resize event listener to window
     window.addEventListener("resize", () => {
+      // if the window width is less than 768px
       if (window.innerWidth < 768 ) {
+        // close the side bar
         this.props.closeSideBar();
       }
     })
