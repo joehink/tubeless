b0fad9c657def18f0ee8cc8169218a3132409268
Joe Hinkley <joehhinkley@gmail.com>
2019-02-17 14:14:38 -0800

    added container to ChannelHeader

--- client/src/components/channel/ChannelHeader.js
+++ client/src/components/channel/ChannelHeader.js
@@ -4,21 +4,23 @@ import SubscriptionButton from "./SubscriptionButton";
 const ChannelHeader = (props) => {
   return (
     <div className="channel-header">
-      {/* channel thumbnail image */}
-      <img
-        className="img-md"
-        src={props.thumbnail}
-        alt={props.title}
-      />
-      <div>
-        {/* name of channel */}
-        <p>{props.title}</p>
-
-        <SubscriptionButton
-          resultId={props.id}
-          thumbnail={props.thumbnail}
-          title={props.title}
+      <div className="container">
+        {/* channel thumbnail image */}
+        <img
+          className="img-md"
+          src={props.thumbnail}
+          alt={props.title}
         />
+        <div>
+          {/* name of channel */}
+          <p>{props.title}</p>
+
+          <SubscriptionButton
+            resultId={props.id}
+            thumbnail={props.thumbnail}
+            title={props.title}
+          />
+        </div>
       </div>
     </div>
   );
