455a7a11336458baa5740f18fd0d29aa2df6ddd8
Joe Hinkley <joehhinkley@gmail.com>
2019-02-19 20:31:37 -0800

    added initial landing styling

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div>
-      Hey landing
-      <a href="/auth/google">Sign In</a>
+    <div className="landing">
+      <div className="landing-content">
+        <img src="./images/logo.png" />
+        <a className="sign-in" href="/auth/google">Sign In</a>
+      </div>
     </div>
   );
 }
